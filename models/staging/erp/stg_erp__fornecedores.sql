0306ee0ff7351e0d855c85c19bad1cb27b790c20
LUIZ PHILIPE VIEIRA <luizphilipe.mv@gmail.com>
2024-11-29 18:05:54 +0000

    Criada a dimensao produto sem documentacao

---
+++ models/staging/erp/stg_erp__fornecedores.sql
@@ -0,0 +1,12 @@
+with
+    renomeado as (
+        select
+            cast(ID as int) as pk_fornecedor
+            , cast(COMPANYNAME as varchar) as nome_fornecedor
+            , cast(CITY as varchar) as cidade_fornecedor
+            , cast(COUNTRY as varchar) as pais_fornecedor
+        from {{ source('erp', 'suppliers') }}
+    )
+
+select *
+from renomeado
