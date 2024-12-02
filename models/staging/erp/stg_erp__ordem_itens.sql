f8f9f8b2abda1ec5fcbcd0a12b642601c89bb915
LUIZ PHILIPE VIEIRA <luizphilipe.mv@gmail.com>
2024-12-02 21:41:50 +0000

    adicionar packages ao projeto e utilizar a ferramenta para gerar uma sk key

--- models/staging/erp/stg_erp__ordem_itens.sql
+++ models/staging/erp/stg_erp__ordem_itens.sql
@@ -3,6 +3,10 @@ with
     renomeado as (
         select
             orderid::varchar || '-' || productid::varchar as pk_pedido_item
+            , {{ dbt_utils.generate_surrogate_key([
+                'orderid'
+                , 'productid'
+                ]) }} as sk_pedido_item
             , cast(orderid as int) as fk_pedido
             , cast(productid as int) as fk_produto
             , cast(discount as numeric(18,2)) as desconto_perc
